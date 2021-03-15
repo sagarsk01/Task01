04957c75ca183792178e0298be914a1acfbcb5c1
sagarsk01 <80068476+sagarsk01@users.noreply.github.com>
2021-03-15 15:50:38 +0530

    Add files via upload

---
+++ file.ps1
@@ -0,0 +1,8 @@
+﻿function getFilesLT5KB{
+Param($FilePath)
+cd $FilePath
+Write-Output "Navigating to $FilePath folder"
+gci -Recurse | Where-Object -FilterScript{$_.Length -lt 5120} |select name
+}
+
+getFilesLT5KB -FilePath 
